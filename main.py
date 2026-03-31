33c8f8189be94300ef30e098ad0f9dbd11d9be2d
Ryan Ramdehol <ravinashwork@gmail.com>
2026-03-31 16:14:40 -0400

    non functional UI setup

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+# ---------------------------- IMPORTS ------------------------------- #
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+FONT_SIZE = 30
+FONT_STYLE = "bold"
+FONT_COLOR = "white"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# ---------------------------- TIMER RESET ------------------------------- #
 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# ---------------------------- UI SETUP ------------------------------- #
+# Initialize the window
+window = Tk()
+window.title("Pomodoro")
+# Padding to the window
+window.config(padx=100, pady=100)
+# Create the canvas using the .png
+canvas = Canvas(window, width=500, height=500)
+# Initialize the image in a PhotoImage class
+tomato_png = PhotoImage(file="tomato.png")
+# Create the image using a create_image module
+canvas.create_image(250, 250, image=tomato_png)
+# Add some text on top the image
+canvas.create_text(250, 270, text="00:00", font=(FONT_NAME, FONT_SIZE, FONT_STYLE), fill=FONT_COLOR)
+canvas.grid(row=0, column=0)
+# Keep window object open so it doesn't close
+window.mainloop()
