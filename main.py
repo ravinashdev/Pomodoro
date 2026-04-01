4e8c87e8f053df7294ba8329549f0da1adde46a1
Ryan Ramdehol <ravinashwork@gmail.com>
2026-04-01 03:31:22 -0400

    logic for checkmarks and need to provide functionality to reset button

--- main.py
+++ main.py
@@ -11,30 +11,39 @@ FONT_NAME = "Courier"
 FONT_SIZE = 30
 FONT_STYLE = "bold"
 FONT_COLOR = "white"
-WORK_SECONDS = 10
-SHORT_BREAK_SECONDS = 20
-LONG_BREAK_SECONDS = 30
+WORK_SECONDS = 1
+SHORT_BREAK_SECONDS = 2
+LONG_BREAK_SECONDS = 3
+# ---------------------------- GLOBAL VARIABLES ------------------------------- #
 REPS = 1
+CHECKS = ""
 # ---------------------------- FUNCTIONS ------------------------------- #
-# ---------------------------- TIMER RESET ------------------------------- #
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
     # print("Start Timer...")
+    # Count the number of times the timer runs to count REPS
     global REPS
     print(REPS)
-    if REPS  == 1 or REPS == 3 or REPS == 5 or REPS == 7:
+    # Work time blocks are at odd REPS
+    if REPS in (1,3,5,7):
         count_down_timer(WORK_SECONDS)
-        timer_label.config(text="Work Time")
+        timer_label.config(text="Work Time", fg=GREEN)
         # create_new_checkmark()
-    if REPS == 2 or REPS == 4 or REPS == 6:
+    # Short break times are at even REPS
+    if REPS in (2,4,6):
+        global CHECKS
         count_down_timer(SHORT_BREAK_SECONDS)
-        timer_label.config(text="Short Break Time")
+        timer_label.config(text="Short Break Time", fg=PINK)
+        CHECKS += "✓"
+        check_mark.config(text=CHECKS)
+    # Long break time is at the 8th REPS
     elif REPS == 8:
         count_down_timer(LONG_BREAK_SECONDS)
-        timer_label.config(text="Long Break Time")
+        timer_label.config(text="Long Break Time", fg=RED)
     REPS += 1
-    # ---------------------------- TIMER MECHANISM ------------------------------- #
-def stop_timer():
-    print("Stop Timer...")
+    # ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    print("Reset Timer...")
     # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down_timer(seconds):
     minutes_left, seconds_left = divmod(seconds, 60)
@@ -44,8 +53,14 @@ def count_down_timer(seconds):
         canvas.grid(row=1, column=1)
         start_button.config(state="disabled")
     elif seconds < 0:
-        timer_label.config(text="Time's Up Hit Start to Continue")
-        start_button.config(state="normal")
+        global REPS, CHECKS
+        if REPS == 8:
+            timer_label.config(text="Time's Up")
+            start_button.config(state="normal")
+            REPS = 0
+            CHECKS = ""
+            check_mark.config(text=CHECKS)
+        start_timer()
 def create_new_checkmark():
     global REPS
     tkinter.Label(text="✓", font=(FONT_NAME, FONT_SIZE, FONT_STYLE), fg=GREEN).grid(row=0, column=REPS).grid(row=3, column=REPS)
@@ -74,17 +89,17 @@ canvas.grid(row=1, column=1)
 # BUTTONS
 # Add a start and stop button
 start_button = Button(text="Start", command=lambda: start_timer())
-stop_button = Button(text="Stop", command=lambda: stop_timer())
+reset_button = Button(text="Stop", command=lambda: reset_timer())
 
 start_button.grid(row=2, column=0)
 stop_button.grid(row=2, column=2)
 
 # LABELS
 # Add a label Timer Title
-timer_label= Label(text="Timer", font=(FONT_NAME, FONT_SIZE, FONT_STYLE), fg=GREEN)
+timer_label= Label(text="Pomodoro Timer", font=(FONT_NAME, FONT_SIZE, FONT_STYLE), fg=GREEN)
 timer_label.grid(row=0, column=1)
 # Add a label Checkmark Symbol
-check_mark = Label(text="✓", font=(FONT_NAME, FONT_SIZE, FONT_STYLE), fg=GREEN)
+check_mark = Label(text=CHECKS, font=(FONT_NAME, FONT_SIZE, FONT_STYLE), fg=GREEN)
 check_mark.grid(row=3, column=1)
 
 # Keep window object open so it doesn't close
