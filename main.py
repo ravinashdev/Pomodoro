f584b5876e78ffb5b2acd92159a91fc5e98f625c
Ryan Ramdehol <ravinashwork@gmail.com>
2026-04-01 14:27:46 -0400

    modify to allow user to input own seconds and show how many pomodoro sets they complete so they can keep track

--- main.py
+++ main.py
@@ -11,17 +11,20 @@ FONT_NAME = "Courier"
 FONT_SIZE = 30
 FONT_STYLE = "bold"
 FONT_COLOR = "white"
-WORK_SECONDS = 1
-SHORT_BREAK_SECONDS = 2
-LONG_BREAK_SECONDS = 3
-TIMER_LOOP = None
+
 # ---------------------------- GLOBAL VARIABLES ------------------------------- #
 REPS = 1
 CHECKS = ""
+TIMER_LOOP = None
+POMODORO_SETS = 0
+
 # ---------------------------- FUNCTIONS ------------------------------- #
-# ---------------------------- TIMER MECHANISM ------------------------------- #
+# ---------------------------TIMER MECHANISM -------------------------- #
 def start_timer():
     # print("Start Timer...")
+    WORK_SECONDS = work_seconds_entry_getter()
+    SHORT_BREAK_SECONDS = short_break_seconds_entry_getter()
+    LONG_BREAK_SECONDS = long_break_seconds_entry_getter()
     # Count the number of times the timer runs to count REPS
     global REPS
     print(REPS)
@@ -43,7 +46,8 @@ def start_timer():
     # ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
     # print("Reset Timer...")
-    global CHECKS, TIMER_LOOP
+    global REPS, CHECKS, TIMER_LOOP
+    REPS = 1
     CHECKS = ""
     check_mark.config(text=CHECKS)
     window.after_cancel(TIMER_LOOP)
@@ -51,23 +55,33 @@ def reset_timer():
     start_button.config(state="normal")
     # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down_timer(seconds):
+    # seconds = minutes * 60
     minutes_left, seconds_left = divmod(seconds, 60)
     if seconds >= 0:
-        global TIMER_LOOP
-        TIMER_LOOP = window.after(1000, count_down_timer, seconds-1)
+        global TIMER_LOOP,POMODORO_SETS
         canvas.itemconfig(timer_digital_clock, text=f" {minutes_left:02}:{seconds_left:02}")
+        TIMER_LOOP = window.after(1000, count_down_timer, seconds - 1)
         canvas.grid(row=1, column=1)
         start_button.config(state="disabled")
     elif seconds < 0:
-        global REPS, CHECKS
+        global REPS, CHECKS, POMODORO_SETS
         if REPS in (1,3,5,7):
             CHECKS += "✓"
             check_mark.config(text=CHECKS)
-        elif REPS == 8:
-            timer_label.config(text="Time's Up")
-            start_button.config(state="normal")
+        elif REPS == 9:
+            POMODORO_SETS += 1
+            timer_label.config(text=f"Time's Up")
+            pomodoro_sets_complete_label.config(text=f"Pomodoro Sets Complete: {POMODORO_SETS}")
         start_timer()
-
+def work_seconds_entry_getter():
+    work_seconds = int(work_seconds_entry.get())
+    return work_seconds
+def short_break_seconds_entry_getter():
+    short_break_seconds = int(short_break_seconds_entry.get())
+    return short_break_seconds
+def long_break_seconds_entry_getter():
+    long_break_seconds = int(long_break_seconds_entry.get())
+    return long_break_seconds
 # ---------------------------- UI SETUP ------------------------------- #
 # Initialize the window
 window = Tk()
@@ -90,10 +104,29 @@ canvas.grid(row=1, column=1)
 
 # BUTTONS
 # Add a start and stop button
-start_button = Button(text="Start", command=lambda: start_timer())
-reset_button = Button(text="Reset", command=lambda: reset_timer())
+start_button = Button(text="Start", command=lambda: start_timer(), font=(FONT_NAME, FONT_SIZE, FONT_STYLE))
+reset_button = Button(text="Reset", command=lambda: reset_timer(), font=(FONT_NAME, FONT_SIZE, FONT_STYLE))
 start_button.grid(row=2, column=0)
 reset_button.grid(row=2, column=2)
+
+# ENTRIES & Entry LABELS
+work_seconds_entry = Entry(width=10, highlightthickness=0)
+work_seconds_entry.insert(END, string="0")
+work_seconds_entry.grid(row=4, column=0)
+work_seconds_entry_label = Label(text="Work Seconds",font=(FONT_NAME, 18, FONT_STYLE))
+work_seconds_entry_label.grid(ipadx=20, ipady=20, row=5, column=0)
+
+short_break_seconds_entry =Entry(width=10, highlightthickness=0)
+short_break_seconds_entry.insert(END, string="0")
+short_break_seconds_entry.grid(row=4, column=1)
+short_break_seconds_entry_label = Label(text="Short Break Seconds",font=(FONT_NAME, 18, FONT_STYLE))
+short_break_seconds_entry_label.grid(ipadx=20, ipady=20, row=5, column=1)
+
+long_break_seconds_entry = Entry(width=10, highlightthickness=0)
+long_break_seconds_entry.insert(END, string="0")
+long_break_seconds_entry.grid(row=4, column=2)
+long_break_entry_label = Label(text="Long Break Seconds",font=(FONT_NAME, 18, FONT_STYLE))
+long_break_entry_label.grid(ipadx=20, ipady=20, row=5, column=2)
 
 # LABELS
 # Add a label Timer Title
@@ -102,6 +135,10 @@ timer_label.grid(row=0, column=1)
 # Add a label Checkmark Symbol
 check_mark = Label(text=CHECKS, font=(FONT_NAME, FONT_SIZE, FONT_STYLE), fg=GREEN)
 check_mark.grid(row=3, column=1)
+# Add a Pomodoro sets complete label
+pomodoro_sets_complete_label = Label(text=f"Pomodoro Sets Complete 3 ✓'s: {POMODORO_SETS}", font=(FONT_NAME, FONT_SIZE, FONT_STYLE), fg=GREEN)
+pomodoro_sets_complete_label.grid(ipadx=20, ipady=20, row=6, column=1)
+
 
 # Keep window object open so it doesn't close
 window.mainloop()
